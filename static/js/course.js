32423daa3ba45562ee506db7a0cf63c418681351
yashan <leeys2423@gmail.com>
2023-12-07 10:44:58 +0800

    add course-related files"

---
+++ static/js/course.js
@@ -0,0 +1,8 @@
+// 將預定課程資料中的日期格式化
+function formatDate(date) {
+  let year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString().padStart(2, "0");
+  let day = date.getDate().toString().padStart(2, "0");
+  let formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
