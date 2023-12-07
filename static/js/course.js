e762178b9c40e580f40a3ead055ea845ff969ab8
yashan <leeys2423@gmail.com>
2023-12-07 11:38:39 +0800

    get course data and render(unfinished)

--- static/js/course.js
+++ static/js/course.js
@@ -1,3 +1,5 @@
+const courseId = window.location.pathname.split("/")[2];
+
 // 將預定課程資料中的日期格式化
 function formatDate(date) {
   let year = date.getFullYear();
@@ -6,3 +8,60 @@ function formatDate(date) {
   let formattedDate = `${year}-${month}-${day}`;
   return formattedDate;
 }
+
+// 驗證登入狀態
+function authenticateUser() {
+  const signinBtnElem = document.querySelector("#nav__signinSignup");
+  if (sessionStorage.getItem("token")) {
+    let token = sessionStorage.getItem("token");
+    let src = "/api/user/auth";
+    let options = {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+    ajax(src, options)
+      .then((data) => {
+        if (data.data != null) {
+          const usernameElem = document.querySelector("#nav__username");
+          const userImgElem = document.querySelector("#nav__user_img");
+          const userImgBtn = document.querySelector(".nav__user_img");
+          const userImgUrl =
+            "https://d277hbzet0a7g8.cloudfront.net/userImage/" + data.data.img;
+          const roleTranslate = {
+            teacher: "老師",
+            student: "同學",
+          };
+          usernameElem.textContent =
+            data.data.name + " " + roleTranslate[data.data.role];
+          usernameElem.classList.remove("elem--hide");
+          userImgElem.setAttribute("src", userImgUrl);
+          userImgBtn.classList.remove("elem--hide");
+        } else {
+          signinBtnElem.classList.remove("elem--hide");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    signinBtnElem.classList.remove("elem--hide");
+  }
+}
+
+// 取得課程資訊
+function getCourseInfo() {
+  const src = "/api/course/" + courseId;
+  const options = {
+    method: "GET",
+  };
+  ajax(src, options).then((data) => {
+    console.log(data);
+  });
+}
+
+(function run() {
+  authenticateUser();
+  getCourseInfo();
+})();
